96b4e7b41c00f5ba2dba603be428ae607cbf7428
tanni6808 <tanni6808450@gmail.com>
2025-01-22 17:36:26 +0800

    task3 first spot

--- week3/script.js
+++ week3/script.js
@@ -3,7 +3,9 @@
 const navIcon = document.querySelector(".nav-burger-menu");
 const navClose = document.querySelector(".nav-close");
 const navContainer = document.querySelector(".nav-container");
+const mainContentGrid = document.querySelector(".main-grid");
 
+///////////////// navigation ///////////////////
 // console.log(navContainer);
 let mql = window.matchMedia("(max-width: 600px)");
 
@@ -35,3 +37,33 @@ navIcon.addEventListener("click", function () {
 navClose.addEventListener("click", function () {
   navContainer.classList.add("hidden");
 });
+
+///////////////// content ///////////////////
+// load & render
+const firstImgUrl = function (filelist) {
+  const lowerFilelist = filelist.toLowerCase();
+  const i = lowerFilelist.indexOf(".jpg");
+  return filelist.slice(0, i + 4);
+};
+
+const initLoadSpotInfo = async function () {
+  const data = await fetch(
+    "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+  )
+    .then((response) => response.json())
+    .then((data) => data.data.results);
+  console.log(data);
+  console.log(data[0].stitle, firstImgUrl(data[0].filelist));
+  const promo = document.createElement("div");
+  promo.classList.add("promo", "promo-1");
+  const promoImg = document.createElement("div");
+  promoImg.classList.add("promo-img");
+  promoImg.style.backgroundImage = `url(${firstImgUrl(data[0].filelist)})`;
+  const promoText = document.createElement("div");
+  promoText.classList.add("promo-text", "text");
+  promoText.innerText = data[0].stitle;
+  promo.append(promoImg, promoText);
+  mainContentGrid.appendChild(promo);
+};
+
+window.addEventListener("load", initLoadSpotInfo);
