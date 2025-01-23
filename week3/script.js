077a5e59e5566dcc7ecec42b38790070f744d7e2
tanni6808 <tanni6808450@gmail.com>
2025-01-23 11:35:07 +0800

    finish task3 4

--- week3/script.js
+++ week3/script.js
@@ -4,6 +4,7 @@ const navIcon = document.querySelector(".nav-burger-menu");
 const navClose = document.querySelector(".nav-close");
 const navContainer = document.querySelector(".nav-container");
 const mainContentGrid = document.querySelector(".main-grid");
+const btnLoadMore = document.querySelector(".btn-load");
 
 ///////////////// navigation ///////////////////
 // console.log(navContainer);
@@ -45,25 +46,70 @@ const firstImgUrl = function (filelist) {
   const i = lowerFilelist.indexOf(".jpg");
   return filelist.slice(0, i + 4);
 };
+let spotList = [];
 
-const initLoadSpotInfo = async function () {
-  const data = await fetch(
-    "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
-  )
-    .then((response) => response.json())
-    .then((data) => data.data.results);
-  console.log(data);
-  console.log(data[0].stitle, firstImgUrl(data[0].filelist));
-  const promo = document.createElement("div");
-  promo.classList.add("promo", "promo-1");
-  const promoImg = document.createElement("div");
-  promoImg.classList.add("promo-img");
-  promoImg.style.backgroundImage = `url(${firstImgUrl(data[0].filelist)})`;
-  const promoText = document.createElement("div");
-  promoText.classList.add("promo-text", "text");
-  promoText.innerText = data[0].stitle;
-  promo.append(promoImg, promoText);
-  mainContentGrid.appendChild(promo);
+const initSpotInfo = async function () {
+  try {
+    const data = await fetch(
+      "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment-1"
+    )
+      .then((response) => response.json())
+      .then((data) => data.data.results);
+
+    let figureNum = 0;
+    for (let i = 0; i < data.length; i++) {
+      if (i < 3) {
+        const promo = document.createElement("div");
+        promo.classList.add("promo", `promo-${i + 1}`);
+        const promoImg = document.createElement("div");
+        promoImg.classList.add("promo-img");
+        promoImg.style.backgroundImage = `url(${firstImgUrl(
+          data[i].filelist
+        )})`;
+        const promoText = document.createElement("div");
+        promoText.classList.add("promo-text", "text");
+        promoText.innerText = data[i].stitle;
+        promo.append(promoImg, promoText);
+        spotList.push(promo);
+      } else {
+        if (figureNum == 10) figureNum = 0;
+        figureNum++;
+        const figure = document.createElement("div");
+        figure.classList.add("figure", `figure-${figureNum}`);
+        figure.style.backgroundImage = `url(${firstImgUrl(data[i].filelist)})`;
+        const figureStar = document.createElement("img");
+        figureStar.classList.add("star-icon");
+        figureStar.src = "star.png";
+        const figureText = document.createElement("div");
+        figureText.classList.add("figure-text", "text");
+        figureText.innerText = data[i].stitle;
+        figure.append(figureStar, figureText);
+        spotList.push(figure);
+      }
+    }
+    for (let i = 0; i < 13; i++) {
+      mainContentGrid.appendChild(spotList[i]);
+    }
+  } catch (err) {
+    btnLoadMore.disabled = true;
+    alert("無法讀取景點資料，請稍後再試。");
+  }
 };
 
-window.addEventListener("load", initLoadSpotInfo);
+const loadMore = function () {
+  // get last rendered spot in main-grid
+  const lastSpotEl = mainContentGrid.lastChild;
+  const startSpotNum = spotList.indexOf(lastSpotEl) + 1;
+  // load 10 more spots
+  for (let i = startSpotNum; i < startSpotNum + 10; i++) {
+    // TODO disable button if there's no more to load
+    if (i == spotList.length) {
+      btnLoadMore.disabled = true;
+      break;
+    }
+    mainContentGrid.appendChild(spotList[i]);
+  }
+};
+
+window.addEventListener("load", initSpotInfo);
+btnLoadMore.addEventListener("click", loadMore);
