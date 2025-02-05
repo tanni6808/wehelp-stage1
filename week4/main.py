78af3e2f60df7cbdf8f39a64b63aa2351c2b6f16
tanni6808 <tanni6808450@gmail.com>
2025-02-05 10:32:31 +0800

    finish task2

--- week4/main.py
+++ week4/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from typing_extensions import Annotated
@@ -13,15 +13,20 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="index.html"
-    )
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/signin")
 async def signin(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-    return {"username": username, "password": password}
+    if username=='test' and password=='test': return RedirectResponse(url='/member', status_code=status.HTTP_302_FOUND)
+    elif username=='' or password=='': return RedirectResponse(url='/error?message=請輸入帳號及密碼', status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url='/error?message=帳號或密碼輸入錯誤', status_code=status.HTTP_302_FOUND)
 
-# @app.get("/member", response_class=HTMLResponse)
-# async def member()
+@app.get("/member", response_class=HTMLResponse)
+async def member(request: Request):
+    return templates.TemplateResponse("member.html", {"request": request})
+
+@app.get("/error")
+async def error(request: Request, message: str):
+    return templates.TemplateResponse("error.html", {"request": request, "message": message})
--- week4/static/script.js
+++ week4/static/script.js
@@ -2,14 +2,12 @@
 
 const agreeCheckboxEl = document.getElementById("agree");
 const btnSignin = document.getElementById("signin");
-const usernameEl = document.getElementById("username");
-const passwordEl = document.getElementById("password");
 
-btnSignin.addEventListener("click", function (e) {
-  //   e.preventDefault();
-  if (agreeCheckboxEl.checked == false) return alert("請勾選「同意條款」！");
-
-  //   usernameEl.value = "";
-  //   passwordEl.value = "";
-  //   agreeCheckboxEl.checked = false;
-});
+if (btnSignin) {
+  btnSignin.addEventListener("click", function (e) {
+    if (agreeCheckboxEl.checked == false) {
+      e.preventDefault();
+      return alert("請勾選「同意條款」！");
+    }
+  });
+}
