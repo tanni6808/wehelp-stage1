dccdc11ad554bcfa74299fc43ae2563cfece5d80
tanni6808 <tanni6808450@gmail.com>
2025-02-18 15:06:01 +0800

    finish sign up, code signin

--- week6/main.py
+++ week6/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse, HTMLResponse
 from typing import Annotated
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='123456',
+    database='website'
+)
 
 app=FastAPI()
 templates=Jinja2Templates(directory='templates')
@@ -18,5 +26,33 @@ async def member(request: Request):
 @app.get('/error')
 async def error(request: Request, message: Annotated[str, None]):
     return templates.TemplateResponse(request=request, name='pages/error.html', context={"message": message})
+
+@app.post('/signup')
+async def signup(name: Annotated[str, Form()], username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    mycursor=mydb.cursor()
+    mycursor.execute('SELECT*FROM member WHERE username = %s', (username, ))
+    myresult=mycursor.fetchall()
+    if myresult==[]:
+        mycursor.execute('INSERT INTO member(name, username, password) VALUE (%s, %s, %s)', (name, username, password))
+        return RedirectResponse(url='/', status_code=status.HTTP_302_FOUND)
+    return RedirectResponse(url=f'/error?message=Repeated username', status_code=status.HTTP_302_FOUND)
+
+@app.post('/signin')
+async def signin(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    mycursor=mydb.cursor()
+    mycursor.execute('SELECT password FROM member WHERE username = %s', (username, ))
+    myresult=mycursor.fetchall()
+    if myresult==[] or myresult[0]!=password:
+        return {'reuslt': myresult}
+    print(myresult)
+    return {'pw': myresult[0]}
+
+@app.get('/test')
+async def test():
+    mycursor=mydb.cursor()
+    mycursor.execute('SELECT*FROM member WHERE username="123456"')
+    myresult=mycursor.fetchall()
+    print(myresult)
+    return {'return': 'OK'}
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
