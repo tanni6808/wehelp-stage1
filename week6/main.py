a953cf276b32f4dff7cd1a69da1cfc387482e524
tanni6808 <tanni6808450@gmail.com>
2025-02-25 14:00:31 +0800

    fix member page redirection

--- week6/main.py
+++ week6/main.py
@@ -20,19 +20,18 @@ app.add_middleware(SessionMiddleware, secret_key='some-random-string')
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    # request.session['user']={}
     return templates.TemplateResponse(request=request, name='pages/index.html')
 
 @app.get('/member')
 async def member(request: Request):
-    if 'user' not in request.session:
+    if 'user' not in request.session or request.session['user']=={}:
         return RedirectResponse(url='/', status_code=status.HTTP_302_FOUND)
-    mycursor=mydb.cursor()
+    mycursor=mydb.cursor(dictionary=True)
     mycursor.execute('SELECT member.name, message.content, message.id FROM message LEFT JOIN member ON message.member_id = member.id;')
     myresult=mycursor.fetchall()
     messages=[]
     for m in myresult:
-        messages.insert(0, {'name':m[0], 'content':m[1], 'deletable': m[0]==request.session['user']['name'], 'id': m[2]})
+        messages.insert(0, {'name':m["name"], 'content':m["content"], 'deletable': m["name"]==request.session['user']['name'], 'id': m["id"]})
     return templates.TemplateResponse(request=request, name='pages/member.html', context={"name": request.session['user']['name'], "messages": messages})
 
 @app.get('/error')
@@ -52,18 +51,17 @@ async def signup(name: Annotated[str, Form()], username: Annotated[str, Form()],
 
 @app.post('/signin')
 async def signin(username: Annotated[str, Form()], password: Annotated[str, Form()], request: Request):
-    mycursor=mydb.cursor()
+    mycursor=mydb.cursor(dictionary=True)
     mycursor.execute('SELECT * FROM member WHERE username = %s', (username, ))
-    myresult=mycursor.fetchall()
-    if myresult==[] or myresult[0][3]!=password:
+    myresult=mycursor.fetchone()
+    if myresult==None or myresult["password"]!=password:
         return RedirectResponse(url=f'/error?message=帳號或密碼輸入錯誤', status_code=status.HTTP_302_FOUND)
-    user_data=myresult[0]
-    request.session['user']={'id': user_data[0], 'username': user_data[2], 'name': user_data[1]}  
+    request.session['user']={'id': myresult['id'], 'username': myresult['username'], 'name': myresult['name']}  
     return RedirectResponse(url='/member', status_code=status.HTTP_302_FOUND)
 
 @app.get('/signout')
 async def signout(request: Request):
-    request.session['user']=[]
+    request.session['user']={}
     return RedirectResponse(url='/', status_code=status.HTTP_302_FOUND)
 
 @app.post('/createMessage')
@@ -75,10 +73,10 @@ async def create_message(message: Annotated[str, Form()], request: Request):
 
 @app.post('/deleteMessage')
 async def delete_message(body: Annotated[str, Body()], request: Request):
-    mycursor=mydb.cursor()
+    mycursor=mydb.cursor(dictionary=True)
     mycursor.execute('SELECT member_id FROM message WHERE id = %s', (body, ))
-    myresult=mycursor.fetchall()
-    if(myresult[0][0]==request.session['user']['id']):
+    myresult=mycursor.fetchone()
+    if(myresult['member_id']==request.session['user']['id']):
         mycursor.execute('DELETE FROM message WHERE id=%s', (body,))
         mydb.commit()
         return {'result': True}
